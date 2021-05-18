0cfb1e0d6b7c920fc810e2690d0bf234dfa075b9
Say <sibarkulana@mail.ru>
2021-05-18 12:56:53 +0300

    add

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+include 'manual/header.php';
+echo $head;
+?>
+<body>
+<div class="grid">
+    <div class="grid_body">
+        <div class="grid_item">1</div>
+        <div class="grid_item">2</div>
+        <div class="grid_item">3</div>
+        <div class="grid_item">4</div>
+        <div class="grid_item">5</div>
+        <div class="grid_item">6</div>
+    </div>
+</div>
+</body>
+
+
+
